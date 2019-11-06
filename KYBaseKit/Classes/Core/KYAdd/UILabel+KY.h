60221d81161c6d832de8949373f6da132ce605d3
longer <504010900@qq.com>
2019-11-06 09:57:10 +0800

    11 pro等新设备宏标识补充

--- KYBaseKit/Classes/Core/KYAdd/UILabel+KY.h
+++ KYBaseKit/Classes/Core/KYAdd/UILabel+KY.h
@@ -11,9 +11,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UILabel (KY)
 
-+ (UILabel *)labelWithText:(NSString * _Nullable)text font:(int)fontSize textColor:(UIColor *)textColor textAlignment:(NSTextAlignment) textAlignment;
++ (UILabel *)labelWithText:(NSString * _Nullable)text font:(int)fontSize textColor:(UIColor * _Nullable)textColor textAlignment:(NSTextAlignment) textAlignment;
 
-+ (UILabel *)labelWithText:(NSString * _Nullable)text blodFont:(int)fontSize textColor:(UIColor *)textColor textAlignment:(NSTextAlignment) textAlignment;
++ (UILabel *)labelWithText:(NSString * _Nullable)text blodFont:(int)fontSize textColor:(UIColor * _Nullable)textColor textAlignment:(NSTextAlignment) textAlignment;
 
 /**
  *  改变行间距
